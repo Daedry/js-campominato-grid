1487cf896bf152775a316f91862aea42c88cf2ab
MyUsername <romamargine@gmail.com>
2022-04-04 17:51:06 +0200

    add function to put numbers in cells and to check if it is even or odd

--- asset/main.js
+++ asset/main.js
@@ -9,42 +9,61 @@ Quando l'utente clicca su ogni cella,
 la cella cliccata si colora di azzurro
 */
 
+//Creo una funzione per la creazione della struttura delle celle
+function generateGrid(selector, cellsNumber, newElement, className) {
+  const cellsElement = document.querySelector(selector);
 
-//Creo una funzione per la creazione della struttura delle celle 
-function generateGrid(selector, cellsNumber, newElement, className ){
-    const cellsElement = document.querySelector(selector);
-
-    for (let i = 1; i <= cellsNumber; i++){
-        const cell = document.createElement(newElement);
-        cell.classList.add(className);
-        cellsElement.append(cell);
-    }
+  for (let i = 1; i <= cellsNumber; i++) {
+    const cell = document.createElement(newElement);
+    cell.classList.add(className);
+    cellsElement.append(cell);
+  }
 }
 
 // console.log(generateGrid('.cells', 100, 'div', 'cell' ));
-generateGrid('.cells', 100, 'div', 'cell' );
+generateGrid(".cells", 100, "div", "cell");
 
-
-
-//Creo una funzione per generare numeri casuali 
-function randomGroupNumbers (min, max){
+//Creo una funzione per generare numeri casuali
+function randomGroupNumbers(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // console.log(generateRandomNum(1,100));
 
 //Creo una funzione per legare i numeri randomici e raggrupparli
-function generateRandomNumbers (min, max){
-    cellsNum = [];
+function generateRandomNumbers(min, max) {
+  cellsNum = [];
 
-    while(cellsNum.length < max){
-        const randomNumber = randomGroupNumbers(min,max);
-        if(!cellsNum.length.includes(randomNumber)){
-            cellsNum.push(randomNumber);
-        }
+  while (cellsNum.length < max) {
+    const randomNumber = randomGroupNumbers(min, max);
+    if (!cellsNum.includes(randomNumber)) {
+      cellsNum.push(randomNumber);
     }
-    return cellsNum;
+  }
+  return cellsNum;
 }
 
 // console.log(generateRandomNumbers(1, 100));
+generateRandomNumbers(1, 100);
 
+//Creo una funzione per mettere i numeri dentro alle celle
+function fillCells(selector, min, max) {
+  const cells = document.querySelectorAll(selector);
+  for (let i = 0; i <= cells.length; i++) {
+    const cell = cells[i];
+    const gridNum = generateRandomNumbers(min, max);
+    cell.innerHTML = `<div>${gridNum[i]}</div>`;
+  }
+}
+
+fillCells(".cell", 1, 100);
+
+
+//Creo una funzione per vedere se il numero è pari o dispari
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
