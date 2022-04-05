268541c0816a692f083f1f1bc881a9c50170a87e
MyUsername <romamargine@gmail.com>
2022-04-05 07:36:09 +0200

    add reset js

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,4 @@
+
 /* 
 L'utente indica un livello di difficoltà 
 in base al quale viene generata una griglia di gioco quadrata, 
@@ -9,61 +10,11 @@ Quando l'utente clicca su ogni cella,
 la cella cliccata si colora di azzurro
 */
 
-//Creo una funzione per la creazione della struttura delle celle
-function generateGrid(selector, cellsNumber, newElement, className) {
-  const cellsElement = document.querySelector(selector);
+// Selezione livello di difficoltà e generazione griglia in un input select
+let selectInput = document.getElementById("selection"); 
 
-  for (let i = 1; i <= cellsNumber; i++) {
-    const cell = document.createElement(newElement);
-    cell.classList.add(className);
-    cellsElement.append(cell);
-  }
-}
+// Griglia di gioco
+let cells = document.querySelector(".cells"); 
 
-// console.log(generateGrid('.cells', 100, 'div', 'cell' ));
-generateGrid(".cells", 100, "div", "cell");
-
-//Creo una funzione per generare numeri casuali
-function randomGroupNumbers(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// console.log(generateRandomNum(1,100));
-
-//Creo una funzione per legare i numeri randomici e raggrupparli
-function generateRandomNumbers(min, max) {
-  cellsNum = [];
-
-  while (cellsNum.length < max) {
-    const randomNumber = randomGroupNumbers(min, max);
-    if (!cellsNum.includes(randomNumber)) {
-      cellsNum.push(randomNumber);
-    }
-  }
-  return cellsNum;
-}
-
-// console.log(generateRandomNumbers(1, 100));
-generateRandomNumbers(1, 100);
-
-//Creo una funzione per mettere i numeri dentro alle celle
-function fillCells(selector, min, max) {
-  const cells = document.querySelectorAll(selector);
-  for (let i = 0; i <= cells.length; i++) {
-    const cell = cells[i];
-    const gridNum = generateRandomNumbers(min, max);
-    cell.innerHTML = `<div>${gridNum[i]}</div>`;
-  }
-}
-
-fillCells(".cell", 1, 100);
-
-
-//Creo una funzione per vedere se il numero è pari o dispari
-function isEven(number){
-    if(number % 2 === 0){
-        return true;
-    } else {
-        return false;
-    }
-}
+// Bottone per iniziare la partita
+let playButton = document.getElementById("btn"); 
